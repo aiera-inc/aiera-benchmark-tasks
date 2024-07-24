9b1881746993d06e26814150413d56421a8d33c3
Jacqueline Garrahan <jacqueline.garrahan@aiera.com>
2024-07-23 17:06:35 -0700

    Check in aiera-ect-sum

---
+++ tasks/aiera-ect-sum/utils.py
@@ -0,0 +1,81 @@
+import datasets
+import numpy as np
+import sacrebleu
+from rouge_score import rouge_scorer, scoring
+
+def process_results_gen(doc, results):
+    completion = results[0]
+    target =  doc["summary"]
+    # BLEU
+    bleu_score = bleu([[target]], [completion])
+
+    # ROUGE-N
+    rouge_scores = rouge([target], [completion])
+
+    # Rouge1_scores
+    rouge1_score = rouge_scores["rouge1"]
+
+    # ROUGE-2
+    rouge2_score = rouge_scores["rouge2"]
+
+    # ROUGE-L
+    rougeL_score = rouge_scores["rougeLsum"]
+
+    return {
+        "bleu": bleu_score,
+        "rouge1": rouge1_score,
+        "rouge2": rouge2_score,
+        "rougeLsum": rougeL_score,
+    }
+
+
+def bleu(refs, preds):
+    """
+    Returns `t5` style BLEU scores. See the related implementation:
+    https://github.com/google-research/text-to-text-transfer-transformer/blob/3d10afd51ba97ac29eb66ae701eca274488202f7/t5/evaluation/metrics.py#L41
+
+    :param refs:
+        A `list` of `list` of reference `str`s.
+    :param preds:
+        A `list` of predicted `str`s.
+    """
+    score = sacrebleu.corpus_bleu(
+        preds,
+        refs,
+        smooth_method="exp",
+        smooth_value=0.0,
+        force=False,
+        lowercase=False,
+        tokenize="intl",
+        use_effective_order=False,
+    ).score
+    return score
+
+
+def rouge(refs, preds):
+    """
+    Returns `t5` style ROUGE scores. See the related implementation:
+    https://github.com/google-research/text-to-text-transfer-transformer/blob/3d10afd51ba97ac29eb66ae701eca274488202f7/t5/evaluation/metrics.py#L68
+
+    :param refs:
+        A `list` of reference `strs`.
+    :param preds:
+        A `list` of predicted `strs`.
+    """
+    rouge_types = ["rouge1", "rouge2", "rougeLsum"]
+    scorer = rouge_scorer.RougeScorer(rouge_types)
+
+    # Add newlines between sentences to correctly compute `rougeLsum`.
+
+    def _prepare_summary(summary):
+        summary = summary.replace(" . ", ".\n")
+        return summary
+
+    # Accumulate confidence intervals.
+    aggregator = scoring.BootstrapAggregator()
+    for ref, pred in zip(refs, preds):
+        ref = _prepare_summary(ref)
+        pred = _prepare_summary(pred)
+        aggregator.add_scores(scorer.score(ref, pred))
+    result = aggregator.aggregate()
+    return {type: result[type].mid.fmeasure for type in rouge_types}
